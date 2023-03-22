1e8bf7904d77f6a633caeaefc5b4d68684ff60fa
joaogpd <joaogdrumond@hotmail.com>
2023-03-21 23:26:07 -0300

    added documentation to operacoes.h

--- tarefa01/operacoes.h
+++ tarefa01/operacoes.h
@@ -2,13 +2,20 @@
 #define OPERACOES_H
 #include "paciente.h"
 
+// tipo de dados paciente
 struct paciente;
+// tipo enum Priority
 enum Priority;
 
+// retorna NULL para criar uma lista encadeada
 struct paciente* cria_lista(void);
+// insere um novo paciente na lista encadeada (mantem ordenacao)
 struct paciente* chegada(struct paciente* l, enum Priority r, int o);
+// imprime todos os elementos da lista encadeada
 void imprime(struct paciente* l);
+// remove o primeiro elemento da lista encadeada 
 struct paciente* atende(struct paciente* l);
+// libera a memoria alocada da lista encadeada
 void libera_lista(struct paciente* l);
 
 #endif
