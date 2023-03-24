f2d495e1abd637c9321263fd1879e636e162bf7e
joaogpd <joaogdrumond@hotmail.com>
2023-03-24 12:44:23 -0300

    improved paciente module documentation

--- tarefa01/paciente.c
+++ tarefa01/paciente.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include "paciente.h"
 
-// Cria uma nova instancia de um struct paciente*. O next tem valor padrao de NULL, 
-// podendo ser alterado na implementacao. Retorna um struct paciente* e recebe um
-// enum Priority e um int 
+/* Cria uma nova instancia de um struct paciente*, alocando a memoria necessaria. Recebe os parametros
+ * um enum Priority, que indica a prioridade do paciente,
+ * um int o, que indica a ordem de chegada do paciente.
+ * Retorna um struct paciente*, um no de lista encadeada
+ * O valor de novo->next e NULL por default, e pode ser alterado na implementacao
+ */
 struct paciente* cria_paciente(enum Priority r, int o) {
     struct paciente* novo = (struct paciente*)malloc(sizeof(struct paciente));
     if (novo == NULL) return NULL;
